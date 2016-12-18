74fdd02154f5660ec9f453be49e71c84654de859
sai koushik <sai koushik>
2016-12-18 22:23:58 +0530

    assignment1.3

---
+++ src/swap.java
@@ -0,0 +1,13 @@
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=9,y=7;
+x=x+y;
+y=x-y;
+x=x-y;
+System.out.println("value is"+x);
+	}
+
+}
